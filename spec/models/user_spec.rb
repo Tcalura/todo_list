b6ab9be5ada880996ffa8d6e88950257bffc001f
Thiago Calura Rossi <thiagocalura@outlook.com>
2019-07-29 17:04:27 -0300

    adds tests for email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe User, type: :model do
   
-  context 'Validates' do
+  context 'Validates Presence' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_presence_of(:password_confirmation) }
+  end
+
+  context 'Validate length of password' do
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
+
+  context 'Validate Email Roles' do
+    it 'Email Regex' do
+      user = build(:user)
+      expect(user.email).to match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/)
+    end
   end
 
   context 'Associations' do
