45e5880c5188eb92a654b38c066be6ee6ff2b126
Thiago Calura Rossi <thiagocalura@outlook.com>
2019-07-29 15:24:13 -0300

    adds validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tasks
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
