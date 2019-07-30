67a34d2ce510100165ffce65b40f9203db4822fb
Thiago Calura Rossi <thiagocalura@outlook.com>
2019-07-29 23:08:17 -0300

    removes presence: true from password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  # validates :password_confirmation, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },
